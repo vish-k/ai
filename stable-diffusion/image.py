1387401b5c5e441aea3a4ab54a05ce1d67214f30
Vish K <vish@Vishs-MacBook-Pro.local>
2025-04-19 16:49:02 -0400

    demo of Stable Diffusion text-to-image diffusion model

---
+++ stable-diffusion/image.py
@@ -0,0 +1,10 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipe = pipe.to("mps")
+
+prompt = "a golden retriver wearing aviator glasses flying a fighter jet "
+image = pipe(prompt).images[0]
+
+image.save("dog-in-air.png")
